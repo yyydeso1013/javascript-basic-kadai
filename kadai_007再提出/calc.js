d2b4d871d8e03cd45203dc61e6447c566a8ad2fd
yyydeso1013 <yyydeso1013@gmail.com>
2025-04-14 23:53:25 +0900

    kadai_007再提出をアップロード

---
+++ kadai_007再提出/calc.js
@@ -0,0 +1,18 @@
+let num = 15
+
+if (num % 3 == 0 && num % 5 == 0) {
+  console.log('3と5の倍数です');
+} 
+
+else if (num % 5 == 0){
+  console.log('5の倍数です');
+}
+
+
+else if (num % 3 == 0) {
+  console.log('3の倍数です');
+}
+
+else {
+  console.log(num);
+}
